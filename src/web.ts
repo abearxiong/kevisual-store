d8d5073542825bb500e4f8b3ebd8495621d69518
xion <xiongxiao1012@outlook.com>
2025-02-19 06:52:18 +0800

    store 添加Load模块

--- src/web.ts
+++ src/web.ts
@@ -3,10 +3,12 @@ import * as WebEnv from './web-env.ts';
 
 import { nanoid } from 'nanoid';
 import * as PathToREgexp from 'path-to-regexp';
+import * as Load from '@kevisual/load/browser';
 
 export const WebModule = {
   Page,
   WebEnv,
   nanoid,
   PathToREgexp,
+  Load,
 };
