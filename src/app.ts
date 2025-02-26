3a027efb794db687bf9417d8c76942f8580c12ce
xion <xiongxiao1012@outlook.com>
2025-02-27 00:57:05 +0800

    feat: add Load

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 // 当前的功能，把所有的模块注入到windows对象当中
-import { useConfigKey, useConfigKeySync, useContextKeySync, useContextKey } from './web-env.ts';
+import * as WebEnv from './web-env.ts';
 import { QueryRouterServer } from '@kevisual/router/browser';
+import * as Load from '@kevisual/load/browser';
 import { Page } from './page.ts';
+
 // bind to window, 必须要的获取全局的环境变量
+const { useConfigKey, useConfigKeySync, useContextKey, useContextKeySync } = WebEnv;
 window.useConfigKey = useConfigKey;
 window.useConfigKeySync = useConfigKeySync;
 window.useContextKey = useContextKey;
 window.useContextKeySync = useContextKeySync;
+// @ts-ignore
+window.webEnv = WebEnv;
+// @ts-ignore
+window.Load = Load;
 window.Page = Page;
 window.QueryRouterServer = QueryRouterServer;
 
