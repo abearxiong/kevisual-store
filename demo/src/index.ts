351b2e336624b05e1b2c9f8b67d9a45cda7ebf9d
xion <xiongxiao1012@outlook.com>
2025-04-03 19:33:16 +0800

    temp

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -1,24 +1,24 @@
-console.log('index.js');
-//@ts-ignore
-import { Page } from '@kevisual/store/page';
+// console.log('index.js');
+// //@ts-ignore
+// import { Page } from '@kevisual/store/page';
 
-const page = new Page({
-  isListen: true,
-});
-page.addPage('/', 'home');
-page.addPage('/:id', 'user');
-page.subscribe(
-  'home',
-  (params, state) => {
-    console.log('home', params, 'state', state);
-    return;
-  },
-);
-page.subscribe('user', (params, state) => {
-  console.log('user', params, 'state', state);
-  return;
-});
+// const page = new Page({
+//   isListen: true,
+// });
+// page.addPage('/', 'home');
+// page.addPage('/:id', 'user');
+// page.subscribe(
+//   'home',
+//   (params, state) => {
+//     console.log('home', params, 'state', state);
+//     return;
+//   },
+// );
+// page.subscribe('user', (params, state) => {
+//   console.log('user', params, 'state', state);
+//   return;
+// });
 
-// page.navigate('/c', { id: 3 });
-// page.navigate('/c', { id: 2 });
-// page.refresh();
+// // page.navigate('/c', { id: 3 });
+// // page.navigate('/c', { id: 2 });
+// // page.refresh();
